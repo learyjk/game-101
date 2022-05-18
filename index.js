938ad4f6360ef3ecbbe4563f303045fab161f168
Keegan <leary.keegan@gmail.com>
2022-05-17 20:42:56 -0700

    explosions

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log(gsap)
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
@@ -67,22 +66,53 @@ class Enemy {
   }
 }
 
+const friction = 0.97;
+class Particle {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+    this.alpha = 1;
+  }
+
+  draw() {
+    c.save();
+    c.globalAlpha = this.alpha;
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+    c.restore();
+  }
+
+  update() {
+    this.draw()
+    this.velocity.x *= friction;
+    this.velocity.y *= friction;
+    this.x = this.x + this.velocity.x
+    this.y = this.y + this.velocity.y
+    this.alpha -= 0.01
+  }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 10, 'white');
 player.draw();
 
+const projectiles = []
+const enemies = []
+const particles = []
+
 function calculateAngle(x1, y1, x2, y2) {
   let diffX = x2 - x1;
   let diffY = y2 - y1;
   let angle = Math.atan2(diffY, diffX) * 180 / Math.PI;
-  console.log(angle);
   return angle;
 }
-
-const projectiles = []
-const enemies = []
 
 function spawnEnemies() {
   setInterval(() => {
@@ -107,9 +137,15 @@ function spawnEnemies() {
 let animationId;
 function animate() {
   animationId = requestAnimationFrame(animate)
-  c.fillStyle = 'rgba(0, 0, 0, 0.1)'
+  c.fillStyle = 'rgba(30, 30, 30, 0.1)'
   c.fillRect(0, 0, canvas.width, canvas.height)
   player.draw();
+  particles.forEach((particle, index) => {
+    if (particle.alpha <= 0.1) {
+      particles.splice(index, 1);
+    }
+    particle.update()
+  })
   projectiles.forEach((projectile, projectileIndex) => {
     projectile.update();
 
@@ -130,9 +166,17 @@ function animate() {
     projectiles.forEach((projectile, projectileIndex) => {
       const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
 
-      // projectile hits enemy touch
+      // projectile hits enemy
       if (dist - enemy.radius - projectile.radius < 1) {
-        if (enemy.radius - 10 > 10) {
+
+        //create explosions
+        for (let i = 0; i < enemy.radius * 2; i++) {
+          particles.push(new Particle(projectile.x, projectile.y, Math.random() * 2, enemy.color, {
+            x: (Math.random() - 0.5) * (Math.random() * 5),
+            y: (Math.random() - 0.5) * (Math.random() * 5),
+          }))
+        }
+        if (enemy.radius - 10 > 5) {
           gsap.to(enemy, { radius: enemy.radius - 10 })
           setTimeout(() => { // setTimeout to wait until next frame to prevent flashing
             projectiles.splice(projectileIndex, 1)
@@ -148,8 +192,8 @@ function animate() {
   });
 }
 
-const projectile = new Projectile(player.x, player.y, 5, 'white', { x: 1, y: 1 });
-projectile.update();
+// const projectile = new Projectile(player.x, player.y, 5, 'white', { x: 1, y: 1 });
+// projectile.update();
 // const projectile2 = new Projectile(player.x, player.y, 5, 'green', { x: 3, y: 3 });
 
 addEventListener('click', (event) => {
